cbb4c937034c374fdce11055e0edf0182081d219
Kyle Hill <kylehill@gmail.com>
2021-01-31 16:48:29 -0500

    Updates part 1 tests to include baseline check

--- puzzles/1/validate.ts
+++ puzzles/1/validate.ts
@@ -10,3 +10,54 @@ export const validatePart2: PuzzleValidateAnswer = (id, answer) => {
   const input = unshuffled(id);
   return answer === (input[2] * input[3] * input[4]).toString();
 };
+
+export const validateFromTextPart1: PuzzleValidateAnswer = (text, answer) => {
+  const lines = text.split("\n");
+  const numbers = lines.map(Number).sort((a, b) => a - b);
+
+  for (let a = 0; a < numbers.length; a++) {
+    let first = numbers[a];
+    for (let b = a + 1; b < numbers.length; b++) {
+      let second = numbers[b];
+
+      if (first + second === 2020) {
+        return (first * second).toString() === answer;
+      }
+
+      if (first + second > 2020) {
+        break;
+      }
+    }
+  }
+
+  return false;
+};
+
+export const validateFromTextPart2: PuzzleValidateAnswer = (text, answer) => {
+  const lines = text.split("\n");
+  const numbers = lines.map(Number).sort((a, b) => a - b);
+
+  for (let a = 0; a < numbers.length; a++) {
+    let first = numbers[a];
+    for (let b = a + 1; b < numbers.length; b++) {
+      let second = numbers[b];
+
+      if (first + second > 2020) {
+        break;
+      }
+      for (let c = b + 1; c < numbers.length; c++) {
+        let third = numbers[c];
+
+        if (first + second + third === 2020) {
+          return (first * second * third).toString() === answer;
+        }
+
+        if (first + second + third > 2020) {
+          break;
+        }
+      }
+    }
+  }
+
+  return false;
+};
--- tests/puzzles/1/validate.test.ts
+++ tests/puzzles/1/validate.test.ts
@@ -1,10 +1,23 @@
-import { validatePart1, validatePart2 } from "puzzles/1/validate";
+import fs from "fs";
+import { create } from "puzzles/1/create";
+import {
+  validatePart1,
+  validatePart2,
+  validateFromTextPart1,
+  validateFromTextPart2,
+} from "puzzles/1/validate";
 
 describe("Day 1 - Validate part 1", () => {
   it("validates part 1", () => {
     expect(validatePart1("1", "801076")).toEqual(true);
     expect(validatePart1("2", "836916")).toEqual(true);
     expect(validatePart1("1", "0")).toEqual(false);
+  });
+
+  it("works on baseline AoC input", async () => {
+    const baseline = await fs.promises.readFile(`${process.cwd()}/puzzles/1/baseline.txt`, "utf8");
+    expect(validateFromTextPart1(baseline, "1007104")).toEqual(true);
+    expect(validateFromTextPart1(create("1"), "801076")).toEqual(true);
   });
 });
 
@@ -13,5 +26,11 @@ describe("Day 1 - Validate part 2", () => {
     expect(validatePart2("1", "137888730")).toEqual(true);
     expect(validatePart2("2", "239754944")).toEqual(true);
     expect(validatePart2("1", "0")).toEqual(false);
+  });
+
+  it("works on baseline AoC input", async () => {
+    const baseline = await fs.promises.readFile(`${process.cwd()}/puzzles/1/baseline.txt`, "utf8");
+    expect(validateFromTextPart2(baseline, "18847752")).toEqual(true);
+    expect(validateFromTextPart2(create("1"), "137888730")).toEqual(true);
   });
 });
